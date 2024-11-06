eda5aa77ac9bf56a85b74a5e8e09258105a687cc
sami-sopas <ramirezcuenca@outlook.es>
2024-11-05 18:22:14 -0600

    add commands file

---
+++ CentroMedico/ProyectoInicial/Comandos.sql
@@ -0,0 +1,13 @@
+PRINT 'HOLA'
+
+SELECT TOP 1 * FROM paciente ORDER BY fnacimiento ASC;
+
+SELECT apellido,MAX(idpaciente) FROM Paciente GROUP BY apellido;
+
+SELECT SUM(idpaciente),apellido from Paciente GROUP BY apellido
+
+SELECT * FROM Paciente
+
+SELECT * FROM Paciente WHERE idPaciente NOT IN(2,4)
+
+SELECT * FROM Paciente WHERE apellido LIKE '%rez%';
